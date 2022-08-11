2953d1ec2d433a3c1805f804c3fdc1e9692c142a
hkk87 <hgkerz@gmail.com>
2022-08-10 23:21:01 -0300

    update tp1

---
+++ pca_check.R
@@ -0,0 +1,32 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(faux)
+
+
+df <- rnorm_multi( n = 100,
+                   mu= c(0,200),
+                   sd = c(1,3),
+                   r= 0.6,
+                   varnames=c("A","B"),
+                   empirical=FALSE)
+
+
+## de easy way
+pca <- prcomp(df,scale=T)
+
+## manual
+df_center= scale(as.matrix(df),center=T,scale=T)
+
+cov_matrix = ( t(df_center) %*% as.matrix(df_center) ) / (dim(df)[1] - 1)
+
+eigs = eigen(cov_matrix)
+
+
+## plots
+
+plot(df_center) +
+  arrows(0,0,pca$rotation[1,],pca$rotation[2,], col="red",lw=2)
+
+plot(df_center) +
+arrows(0,0,eigs$vectors[1,],eigs$vectors[2,], col="blue",lw=2)
--- tp1/tp1.R
+++ tp1/tp1.R
@@ -4,98 +4,126 @@ library(dplyr)
 library(tidyr)
 library(visdat)
 library(reshape)
+library(gridExtra)
+
+is_outlier <- function(data){
+  iqr = IQR(data)
+  quantiles = quantile(data, probs=c(.25,.75))
+  return (data < quantiles[1] - (1.5 * iqr) |
+                       data > quantiles[2] + (1.5 * iqr))
+}
 
 df <- read.csv("C:/Users/hgker/Desktop/master_ds/fundamentos_r/tp1/europe.csv")
 
 df <- df %>% 
-        mutate(Area = Area / 100000,
-               GDP = GDP / 1000)
+      mutate(Area = Area / 100000,
+               GDP = GDP / 1000) 
 
-vis_miss(df) + labs(caption="Fig 1. Missing Data.")
-vis_dat(df) + labs(caption="Fig 1. Missing Data y tipo de datos en el dataset.")
+# Mahalanobis Dist
+
+df_with_dist <- df %>% 
+    select_if(is.numeric) %>% 
+    mutate(Dist_Mahalanobis = mahalanobis(.,colMeans(.),cov(.))) %>% 
+    left_join(df) %>% 
+    select("Country","Dist_Mahalanobis","Area","GDP",
+           "Inflation","Life.expect","Military","Pop.growth","Unemployment") 
 
 
-ggplot(melt(df),
-       aes(x=1, y=value)) +
+# # Mahalanobis Dist outliers table
+# 
+# df_with_dist %>% 
+#   arrange(Dist_Mahalanobis) %>% 
+#   filter(is_outlier(Dist_Mahalanobis)) %>% 
+#   tableGrob(.) %>% 
+#   grid.arrange(.)
+# 
+# 
+# # Boxplot Maha dist
+# 
+# ggplot(df_with_dist,aes(x=1,y=Dist_Mahalanobis)) +
+# 
+#   geom_boxplot() +
+#   theme(axis.text.x=element_blank(),
+#         axis.ticks.x=element_blank()) +
+#   labs(title="boxplot",
+#       caption="Figgfg")
+
+scatter_1 <- ggplot(df %>% filter(!Country %in% c("Spain","Luxembourg")),aes(x=Unemployment,y=GDP)
+                    ) +
+              geom_point(colour="navy"
+                         ) +
+              geom_point(data=df %>% filter(Country == "Spain"), 
+                         colour="darkorange",
+                         size=3
+                         ) +
+              geom_point(data=df %>% filter(Country == "Luxembourg"), 
+                         colour="magenta",
+                         size=3
+                         ) 
+
+scatter_2 <- ggplot(df %>% filter(!Country %in% c("Ukraine","Luxembourg")),aes(x=Inflation,y=GDP)
+                        ) +
+              geom_point(colour="navy"
+                         ) +
+              geom_point(data=df %>% filter(Country == "Ukraine"), 
+                         colour="red",
+                         size=3,show.legend = TRUE
+                         ) +
+              geom_point(data=df %>% filter(Country == "Luxembourg"), 
+                         colour="magenta",
+                         size=3
+                         ) 
+
+boxplot_out <- ggplot(df_with_dist,aes(x=Dist_Mahalanobis,y=1)) +
   
-    geom_boxplot(fill="grey",
-                 outlier.color="dark red") +
-    facet_wrap(~variable, scales="free") + 
-    theme(axis.text.x=element_blank(),
-          axis.ticks.x=element_blank())
+  geom_boxplot() +
+  theme(axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) +
+  labs(title="boxplot",
+       caption="Figgfg") 
+
+grid.arrange(
+             boxplot_out,
+             arrangeGrob(scatter_1,
+                         scatter_2,ncol=2),
+
+             top="Mahalanobis Distance Outliers")
 
 
-ggpairs(df,
-        columns = 2:8,
-        lower=list(continuous=wrap("points",colour="navy")),
-        diag=list(continuous=wrap(ggally_box_no_facet, 
-                                  colour = "dark red", 
-                                  fill="grey",
-                                  outlier.color="dark red")
-                  )
-        ) +
-  labs(caption="Fig 2. Exploratory data analysis")
-  
 
+# vis_miss(df) + 
+#   labs(caption="Fig 1. Missing Data.")
 # 
-# region <- function(country) {
+# vis_dat(df) + 
+#   labs(caption="Fig 1. Missing Data y tipo de datos en el dataset.")
+# 
+# 
+# ggplot(melt(df),
+#        aes(x=1, y=value)) +
 #   
-#   east_europe <- c("Bulgaria","Czech Republic","Hungary","Poland","Slovakia","Ukraine")
-#   north_europe <- c("Denmark","Estonia","Finland","Iceland","Ireland","Latvia","Lithuania","Norway","Sweden","United Kingdom")
-#   south_europe <- c("Croatia","Greece","Italy","Portugal","Slovenia","Spain")
-#   ##west_europe <- c("Austria","Belgium","Germany","Luxembourg","Netherlands","Switzerland")
+#     geom_boxplot(fill="grey",
+#                  outlier.color="dark red") +
+#     facet_wrap(~variable, scales="free") + 
+#     theme(axis.text.x=element_blank(),
+#           axis.ticks.x=element_blank())
+# 
+# 
+# ggpairs(df,
+#         columns = 2:8,
+#         lower=list(continuous=wrap("points",colour="navy")),
+#         diag=list(continuous=wrap(ggally_box_no_facet, 
+#                                   colour = "dark red", 
+#                                   fill="grey",
+#                                   outlier.color="dark red")
+#                   )
+#         ) +
+#   labs(caption="Fig 2. Exploratory data analysis")
 #   
-#   if (country %in% east_europe) return("Eastern Europe") 
-#   if (country %in% north_europe) return("Northen Europe") 
-#   if (country %in% south_europe) return("Southern Europe") 
-#   else return("Western Europe")
-# }
 # 
-# df_region <- df %>% 
-#   rowwise() %>% 
-#   mutate(Region = factor(region(Country)))
-# 
-# df_region %>% select(Country, Region) %>% count(Region)
+# ggcorr(df, label = T) +
+#   labs(title= "Matriz de correlacion",
+#       caption="Fig 3. Matriz de correlacion.")
 
 
 
 
-# df_t <- data.frame(t(df))
-# names(df_t) <- df_t[1,]
-# df_t <- df_t[-1,]
-
-
-
-# ggplot(df,aes(Military,GDP, color = Region, size=Unemployment)) +
-#   geom_point() +
-#   scale_x_log10()
-
-
-
-# ###### graph with corr color
-# 
-# my_fn <- function(data, mapping, method="p", use="pairwise", ...){
-# 
-#   # grab data
-#   x <- eval_data_col(data, mapping$x)
-#   y <- eval_data_col(data, mapping$y)
-# 
-#   # calculate correlation
-#   corr <- cor(x, y, method=method, use=use)
-# 
-#   # calculate colour based on correlation value
-#   # Here I have set a correlation of minus one to blue,
-#   # zero to white, and one to red
-#   # Change this to suit: possibly extend to add as an argument of `my_fn`
-#   colFn <- colorRampPalette(c("navy", "white", "red"), interpolate ='spline')
-#   fill <- colFn(100)[findInterval(corr, seq(-1, 1, length=100))]
-# 
-#   ggally_cor(data = data, mapping = mapping, ...) +
-#     theme_void() +
-#     theme(panel.background = element_rect(fill=fill))
-# }
-# 
-# ggpairs(df_region,columns = 2:8,
-#         upper = list(continuous = my_fn))
-# # lower = list(continuous = "smooth"))
-# 
